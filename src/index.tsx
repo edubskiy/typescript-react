9faf8c31058f65a8b1fb79822b1917e468c9f500
Evgeniy <edubskiy@openwaygroup.com>
2020-02-09 16:48:07 +0300

    Create store with thunk middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-interface AppProps {
-  color?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = { counter: 0 };
-  }
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 })
-  }
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 })
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider>
+    <App color="red" />
+  </Provider>,
+  document.querySelector('#root')
+);
