5c0d87d908c4f1b8cbd3c55b41dd95ea26c8c8fc
Evgeniy <edubskiy@openwaygroup.com>
2020-02-09 20:53:58 +0300

    Wire up redux to connect App component with global store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
+import { connect } from 'react-redux';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+export class _App extends React.Component<AppProps> {
+  componentDidMount() {
+    const { fetchTodos } = this.props;
+
+    fetchTodos();
+  }
+
   render() {
     return <div>Hi there!</div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { 
+    todos 
+  }
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
