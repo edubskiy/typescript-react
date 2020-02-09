1e6e0bd16aa60871f9e94ba836bea521dd016a14
Evgeniy <edubskiy@openwaygroup.com>
2020-02-09 16:38:02 +0300

    Basic React App example with initial state setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 interface AppProps {
-  color: string;
+  color?: string;
 }
 
-class App extends React.Component<AppProps> {
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
+
   render() {
-    return <div>{this.props.color}</div>
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
