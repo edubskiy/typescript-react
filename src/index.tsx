12841f259c310737a5a04527a5fb365ed7870cfc
Evgeniy <edubskiy@openwaygroup.com>
2020-02-09 16:57:44 +0300

    Added type definitions for react and react-dom packages via @types/...

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
