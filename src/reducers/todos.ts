bb0bed7be7158da6d9786e10fd356a19da4304cb
Evgeniy <edubskiy@openwaygroup.com>
2020-02-10 11:27:49 +0300

    Reorganize action files

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,5 +1,4 @@
-import { ActionTypes } from '../actions/types';
-import { Todo, FetchTodosAction } from '../actions';
+import { Todo, FetchTodosAction, ActionTypes } from '../actions';
 
 export const todosReducer = (
   state: Todo[] = [],
